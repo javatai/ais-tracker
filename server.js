189ca4a19d7c0a62982bfd7f5851f2541cf37362
Marcel Bretschneider <marcel.bretschneider@gmail.com>
2015-10-17 12:24:39 +0200

    Add Server logs

--- server.js
+++ server.js
@@ -3,10 +3,31 @@
 var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + '/config/config.json')[env];
 
-var sequelize = require('./lib/init');
+var FileStreamRotator = require('file-stream-rotator');
+var fs = require('fs');
 
 var express = require('express');
+var morgan = require('morgan');
+
 var app = express();
+var sequelize = require('./lib/init');
+
+// create a write stream (in append mode)
+var logDirectory = __dirname + '/logs'
+
+// ensure log directory exists
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+
+// create a rotating write stream
+var accessLogStream = FileStreamRotator.getStream({
+  filename: logDirectory + '/access-%DATE%.log',
+  frequency: 'daily',
+  verbose: false,
+  date_format: "YYYY-MM-DD"
+})
+
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
 
 // Create REST resource
 require('./api/ship')(app);
